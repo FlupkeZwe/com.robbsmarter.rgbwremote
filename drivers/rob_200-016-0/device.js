78151a88252ccda115e4b9d8e2215c14fb66c624
FlupkeZwe <frits@zwegers.net>
2022-10-16 12:51:41 +0200

    Add group identification to bound clusters
    in order to prepare to phase out the sub-devices

--- drivers/rob_200-016-0/device.js
+++ drivers/rob_200-016-0/device.js
@@ -33,24 +33,29 @@ class RGBWRemoteControl extends ZigBeeDevice {
         debug(true);
         this.enableDebug();
         zclNode.endpoints[this._getGroupNumber()].bind(CLUSTER.ON_OFF.NAME, new OnOffBoundCluster({
+            groupId: this._getGroupNumber(),
             onToggle: this._toggleCommandHandler.bind(this),
             onSetOn: this._toggleCommandHandler.bind(this),
             onSetOff: this._toggleCommandHandler.bind(this)
         }));
         zclNode.endpoints[this._getGroupNumber()].bind(CLUSTER.LEVEL_CONTROL.NAME, new LevelControlBoundCluster({
+            groupId: this._getGroupNumber(),
             stepWithOnOff: this._levelControlCommandHandler.bind(this)
         }));
         zclNode.endpoints[this._getGroupNumber()].bind(CLUSTER.COLOR_CONTROL.NAME, new ColorControlBoundCluster({
+            groupId: this._getGroupNumber(),
             moveToColor: this._moveToColorCommandHandler.bind(this),
             moveToColorTemperature: this._colorControlCommandHandler.bind(this)
         }));
 
         zclNode.endpoints[this._getGroupNumber()].bind(CLUSTER.SCENES.NAME, new ScenesBoundCluster({
+            groupId: this._getGroupNumber(),
             recallScene: this._scenesCommandHandler.bind(this),
             storeScene: this._scenesCommandHandler.bind(this)
         }));
 
         zclNode.endpoints[this._getGroupNumber()].bind(CLUSTER.BASIC.NAME, new BasicBoundCluster({
+            groupId: this._getGroupNumber(),
             factoryReset: this._basicCommandHandler.bind(this),
             recallScene: this._scenesCommandHandler.bind(this)
         }));
@@ -58,15 +63,15 @@ class RGBWRemoteControl extends ZigBeeDevice {
 
     }
 
-    _toggleCommandHandler() {
-        this.log('Command handler triggered ' + this._getGroupNumber());
-        this.triggerFlow({id: 'toggled'})
+    _toggleCommandHandler({groupId}) {
+        this.log('Command handler triggered ' + groupId);
+        this.triggerFlow({id: 'toggled', args: {}, state: {groupId: groupId}})
         .then(() => this.log('flow was triggered', 'toggled'))
          .catch (err => this.error('Error: triggering flow', 'toggled', err));
     }
 
-    _levelControlCommandHandler({mode, stepSize, transitionTime}) {
-        this.log('LC Command handler triggered ' + this._getGroupNumber() + ' mode ' + mode + ' stepSize ' + stepSize + ' tranTime ' + transitionTime);
+    _levelControlCommandHandler({groupId, mode, stepSize, transitionTime}) {
+        this.log('LC Command handler triggered ' + groupId + ' mode ' + mode + ' stepSize ' + stepSize + ' tranTime ' + transitionTime);
         if (mode == 1) { //up
             this.triggerFlow({id: 'dim_up'})
             .then(() => this.log('flow was triggered', 'dim_up'))
@@ -81,11 +86,11 @@ class RGBWRemoteControl extends ZigBeeDevice {
         
     }
 
-    _colorControlCommandHandler() {
-        this.log('CC Command handler triggered ' + this._getGroupNumber());
+    _colorControlCommandHandler({groupId}) {
+        this.log('CC Command handler triggered ' + groupId);
     }
     
-    _moveToColorCommandHandler({colorX, colorY}) {
+    _moveToColorCommandHandler({groupId, colorX, colorY}) {
         this.log('Moving to color input X:' + colorX + ' Y:' + colorY);
         
         const rgb = ColorConverter.xy2rgb(colorX / 65536, colorY / 65536);
@@ -101,14 +106,14 @@ class RGBWRemoteControl extends ZigBeeDevice {
     _scenesCommandHandler({
         groupId, sceneId
     }) {
-        this.log('Scenes Command handler triggered ' + this._getGroupNumber() + ' Group ' + groupId + ' Scene ' + sceneId);
+        this.log('Scenes Command handler triggered ' + ' Group ' + groupId + ' Scene ' + sceneId);
         this.triggerFlow({id: 'recall_scene', tokens: {}, state: {sceneId: sceneId}})
             .then(() => this.log('flow was triggered', 'recall_scene'))
          .catch (err => this.error('Error: triggering flow', 'recall_scene', err));
     }
 
-    _basicCommandHandler() {
-        this.log('Basic Command handler triggered ' + this._getGroupNumber() + 'scene ' + sceneId);
+    _basicCommandHandler({groupId}) {
+        this.log('Basic Command handler triggered ' + groupId);
         // No action
     }
 }
