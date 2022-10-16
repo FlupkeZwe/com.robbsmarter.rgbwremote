7c520e43b11b2ac4ff1ff19921ca4dc44f92bb2a
FlupkeZwe <frits@zwegers.net>
2022-10-16 08:14:24 +0200

    ColorControl moveToColor working
    Created a color converter to convert the xyY color space to RGB (using wide D65 RGB conversion) and HSV

--- drivers/rob_200-016-0/device.js
+++ drivers/rob_200-016-0/device.js
@@ -12,6 +12,7 @@ const LevelControlBoundCluster = require('../../lib/LevelControlBoundCluster');
 const ColorControlBoundCluster = require('../../lib/ColorControlBoundCluster');
 const ScenesBoundCluster = require('../../lib/ScenesBoundCluster');
 const BasicBoundCluster = require('../../lib/BasicBoundCluster');
+const ColorConverter = require('../../lib/ColorConverter');
 
 class RGBWRemoteControl extends ZigBeeDevice {
 
@@ -29,9 +30,8 @@ class RGBWRemoteControl extends ZigBeeDevice {
     }) {
         this.log('Init rgbw device class' + this._getGroupNumber());
         // Bind on/off button commands
-        //debug(true);
-
-
+        debug(true);
+        this.enableDebug();
         zclNode.endpoints[this._getGroupNumber()].bind(CLUSTER.ON_OFF.NAME, new OnOffBoundCluster({
             onToggle: this._toggleCommandHandler.bind(this),
             onSetOn: this._toggleCommandHandler.bind(this),
@@ -41,7 +41,7 @@ class RGBWRemoteControl extends ZigBeeDevice {
             stepWithOnOff: this._levelControlCommandHandler.bind(this)
         }));
         zclNode.endpoints[this._getGroupNumber()].bind(CLUSTER.COLOR_CONTROL.NAME, new ColorControlBoundCluster({
-            moveToColor: this._colorControlCommandHandler.bind(this),
+            moveToColor: this._moveToColorCommandHandler.bind(this),
             moveToColorTemperature: this._colorControlCommandHandler.bind(this)
         }));
 
@@ -49,6 +49,7 @@ class RGBWRemoteControl extends ZigBeeDevice {
             recallScene: this._scenesCommandHandler.bind(this),
             storeScene: this._scenesCommandHandler.bind(this)
         }));
+
         zclNode.endpoints[this._getGroupNumber()].bind(CLUSTER.BASIC.NAME, new BasicBoundCluster({
             factoryReset: this._basicCommandHandler.bind(this),
             recallScene: this._scenesCommandHandler.bind(this)
@@ -64,22 +65,51 @@ class RGBWRemoteControl extends ZigBeeDevice {
          .catch (err => this.error('Error: triggering flow', 'toggled', err));
     }
 
-    _levelControlCommandHandler() {
-        this.log('LC Command handler triggered ' + this._getGroupNumber());
+    _levelControlCommandHandler({mode, stepSize, transitionTime}) {
+        this.log('LC Command handler triggered ' + this._getGroupNumber() + ' mode ' + mode + ' stepSize ' + stepSize + ' tranTime ' + transitionTime);
+        if (mode == 1) { //up
+            this.triggerFlow({id: 'dim_up'})
+            .then(() => this.log('flow was triggered', 'dim_up'))
+         .catch (err => this.error('Error: triggering flow', 'dim_up', err));
+        } else {
+            this.triggerFlow({id: 'dim_down'})
+            .then(() => this.log('flow was triggered', 'dim_down'))
+         .catch (err => this.error('Error: triggering flow', 'dim_down', err));
+        }
+        
+        
+        
     }
 
     _colorControlCommandHandler() {
         this.log('CC Command handler triggered ' + this._getGroupNumber());
+    }
+    
+    _moveToColorCommandHandler({colorX, colorY}) {
+        this.log('Moving to color input X:' + colorX + ' Y:' + colorY);
+        
+        const rgb = ColorConverter.xy2rgb(colorX / 65536, colorY / 65536);
+        this.log('Calculated RGB R:' + rgb.r + ' G:' + rgb.g + ' B:' + rgb.b);
+        
+        const hsv = ColorConverter.xy2hsv(colorX / 65536, colorY / 65536);
+        this.log('Calculated HSV H:' + hsv.h + ' S:' + hsv.s + ' V:' + hsv.v);
+        this.triggerFlow({id: 'color_hsv_moved', tokens: {hue: hsv.h / 360, saturation: hsv.s, value: hsv.v}, state: {}})
+            .then(() => this.log('flow was triggered', 'color_hsv_moved'))
+         .catch (err => this.error('Error: triggering flow', 'color_hsv_moved', err));
     }
 
     _scenesCommandHandler({
         groupId, sceneId
     }) {
         this.log('Scenes Command handler triggered ' + this._getGroupNumber() + ' Group ' + groupId + ' Scene ' + sceneId);
+        this.triggerFlow({id: 'recall_scene', tokens: {}, state: {sceneId: sceneId}})
+            .then(() => this.log('flow was triggered', 'recall_scene'))
+         .catch (err => this.error('Error: triggering flow', 'recall_scene', err));
     }
 
-    _basicCommandHandler(sceneId) {
+    _basicCommandHandler() {
         this.log('Basic Command handler triggered ' + this._getGroupNumber() + 'scene ' + sceneId);
+        // No action
     }
 }
 
--- drivers/rob_200-016-0/firstGroup.device.js
+++ drivers/rob_200-016-0/firstGroup.device.js
@@ -3,9 +3,23 @@
 const RGBWRemoteControl = require('./device.js');
 
 class FirstGroupDevice extends RGBWRemoteControl {
-  
-  _getGroupNumber() {
-    return 1;
-  }
+
+    _getGroupNumber() {
+        return 1;
+    }
+
+    async onNodeInit({
+        zclNode
+    }) {
+
+        const recallSceneTriggerCard = this.homey.flow.getDeviceTriggerCard('recall_scene');
+        recallSceneTriggerCard.registerRunListener(async(args, state) => {
+            return args.scene_name == state.sceneId;
+        });
+
+        super.onNodeInit({
+            zclNode
+        });
+    }
 }
 module.exports = FirstGroupDevice;
